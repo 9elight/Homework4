676345f1580ed6c13c46163d049a9ff9c118c0c0
9elight <53308315+9elight@users.noreply.github.com>
2019-07-31 21:10:22 +0600

    homework

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,9 @@ package com.company;
 import java.util.Random;
 
 public class Main {
-    public static int[] heatpoint = {700, 250, 250, 250, 250};
-    public static int[] damage = {50, 20, 20, 20, 10};
-    public static String[] hitTypes = {"Physical", "Physical", "Magical", "Archer", "Medical"};
+    public static int[] heatpoint = {900, 250, 250, 250, 500,250};
+    public static int[] damage = {60, 20, 20, 20, 5, 10};
+    public static String[] hitTypes = {"Physical", "Physical", "Magical", "Archer","Tank", "Medical"};
 
     public static void main(String[] args) {
         roundStat();
@@ -19,11 +19,14 @@ public class Main {
 
     public static void round() {
 
-        for (int i = 1; i <= 4; i++) {
+        for (int i = 1; i <= 5; i++) {
+            if (heatpoint[i] > 0){
 
-            heatpoint[i] = bossDamage(i);
+            heatpoint[i] = bossDamage(i);}
+            else heatpoint[i] = 0;
+
         }
-        for (int i = 1; i <= 3; i++) {
+        for (int i = 1; i <= 4; i++) {
             int healthRemain = heroesDamage(i);
             if (healthRemain < 0) {
                 heatpoint[0] = 0;
@@ -42,7 +45,7 @@ public class Main {
             System.out.println("Heroes won");
             return true;
         }
-        if (heatpoint[1] <= 0 && heatpoint[2] <= 0 && heatpoint[3] <= 0 && heatpoint[4] <= 0) {
+        if (heatpoint[1] <= 0 && heatpoint[2] <= 0 && heatpoint[3] <= 0 && heatpoint[4] <= 0 && heatpoint[5] <= 0) {
             System.out.println("Boss won");
             return true;
         }
@@ -50,14 +53,18 @@ public class Main {
     }
 
     public static int bossDamage(int playerInd) {
-        return heatpoint[playerInd] - damage[0];
+       if (playerInd != 4){
+           if (heatpoint[4] >= 0){
+        return heatpoint[playerInd] - damage[0]/2;}}
+       else { return heatpoint[playerInd] - damage[0];}
+       return heatpoint[playerInd] - damage[0];
     }
 
     public static int heroesDamage(int playerInd) {
 
         if (hitTypes[0].equals(hitTypes[playerInd])) {
             Random r = new Random();
-            int randomCrit = r.nextInt(7) + 2;
+            int randomCrit = r.nextInt(4) + 2;
             System.out.println(hitTypes[playerInd] + " damage boss " + damage[playerInd] * randomCrit);
             return heatpoint[0] - damage[playerInd] * randomCrit;
 
@@ -71,7 +78,7 @@ public class Main {
         if (heatpoint[4] > 0){
 
         System.out.println("Medic heal " + hitTypes[randomHeroes]);
-        return heatpoint[randomHeroes] + damage[4];
+        return heatpoint[randomHeroes] + damage[5];
 
     }
         return heatpoint[randomHeroes];
@@ -83,13 +90,14 @@ public class Main {
         System.out.println("Warior heatpoints " + heatpoint[1]);
         System.out.println("Magician heatpoints " + heatpoint[2]);
         System.out.println("Archer heatpoints " + heatpoint[3]);
-        System.out.println("Medic heatpoints " + heatpoint[4]);
+        System.out.println("Tank heatpoints " + heatpoint[4]);
+        System.out.println("Medic heatpoints " + heatpoint[5]);
         System.out.println("_____________________");
     }
 
     public static void bossDefence() {
         Random r = new Random();
-        int randomNumb = r.nextInt(3) + 1;
+        int randomNumb = r.nextInt(5) + 1;
         hitTypes[0] = hitTypes[randomNumb];
 
     }
